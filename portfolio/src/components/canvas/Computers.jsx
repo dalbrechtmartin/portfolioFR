152e6ef5c03a196522fff040e44b23884059312f
Danaé ALBRECHT--MARTIN <danae.albrecht-martin@etu.unistra.fr>
2023-11-24 17:26:15 +0100

    Adding some parameter

--- portfolio/src/components/canvas/Computers.jsx
+++ portfolio/src/components/canvas/Computers.jsx
@@ -12,14 +12,25 @@ const Computers = () => {
       <hemisphereLight intensity={0.15} 
       groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
 }
 
-{/* https://youtu.be/0fYi8SGA20k?si=dCS1bJ0Iasl-Uh1w&t=3260 */}
+{/* https://youtu.be/0fYi8SGA20k?si=C_g7PndSNsJqhXny&t=3408 */}
 
 const ComputersCanvas = () => {
   return (
